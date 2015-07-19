e990bf545c1e4ee797826e9e26183b978fbc46e4
Todd Kronenberg <toddkronenberg@gmail.com>
2015-07-19 12:02:24 -0700

    Implemented gulp, made a few small related changes

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    copy = require('gulp-copy');
+
+// default task to run watch task on gulp command
+gulp.task('default', ['watch']);
+// run build to just run gulp tasks without watching
+gulp.task('build', ['jshint', 'build-css', 'build-js']);
+
+// check for javscript errors in server and client
+gulp.task('jshint', function() {
+  return gulp.src(['./client/**/*.js',
+                   './server/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// build css from sass
+gulp.task('build-css', function() {
+  return gulp.src('./client/assets/styles/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/assets/styles'));
+});
+
+// bundle js into one file on client, uglify if production is set
+gulp.task('build-js', function() {
+  // return gulp.src(['./client/app/**/*.js',
+  //                  './client/components/**/*.js',
+  //                  './client/*.js'])
+  //   .pipe(sourcemaps.init())
+  //   .pipe(concat('bundle.js'))
+  //   // only uglify if gulp is ran with '--type production'
+  //   .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+  //   .pipe(sourcemaps.write())
+  //   .pipe(gulp.dest('./public/js'));
+});
+
+// copy client folder to public folder
+gulp.task('copy', function() {
+  return gulp.src('./client/**/*')
+    .pipe(copy('./public', {prefix: 1}));
+});
+
+// watch task
+gulp.task('watch', function() {
+  gulp.watch(['./client/**/*.js'], ['build-js']);
+  gulp.watch(['./client/**/*.js', './server/**/*.js'], ['jshint']);
+  gulp.watch('./client/assets/styles/**/*.scss', ['build-css']);
+  gulp.watch('./client/**/*', ['copy']);
+});
--- server/app.js
+++ server/app.js
@@ -9,8 +9,8 @@ var app = express();
 // running some basic Express middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(__dirname + './../client'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + './../public'));
 
 
 // middleware
